7ecdcd63908b84434d49631529f91ff1d4f2f753
Kirstin Kleyn <57179572+kmkleyn@users.noreply.github.com>
2020-08-13 12:15:26 +0200

    Update submitreview.php
    Add Bootstrap styling and other minor edits.

--- submitrevirew.php
+++ submitrevirew.php
@@ -3,42 +3,69 @@
     require_once 'includes/header.inc.php';
 ?>
 
+<h1>Submit a Review</h1>
+<!-- This form collects the review information from the user and uses the POST method in 
+includes/submit.inc.php to save it to the database. -->
 <form action="" method="POST">
 
-    <label>Name</label>
-    <input type="text" name="customerName" placeholder="Name">
+    <!-- Customer Name -->
+    <div class="form-group">
+        <label>Your name</label>
+        <input type="text" name="customerName" placeholder="Name">
+        <br>
+    </div>
 
-    <label>Email</label>
-    <input type="text" name="customerEmail" placeholder="Email">
+    <!-- Customer Email -->
+    <div class="form-group">
+        <label>Your email</label>
+        <input type="text" name="customerEmail" placeholder="Email">
+        <br>
+    </div>
+
+    <!-- Product ID
+    This loops through the products in the products table to display their IDs.
+    An improvement can be made here to display the product names instead. -->
+    <div class="form-group">
+        <label>Product</label>
+        <select name="productID">
+
+            <?php 
+                $result = $conn->query("SELECT productID FROM products") or die($conn->error());
+                while ($row = $result->fetch_assoc()):
+            ?>
+
+            <option><?php echo $row['productID']?></option>
+            
+            <?php endwhile; ?>
+
+        </select>
+        <br>
+    </div>
+
+    <!-- Review Rating -->
+    <div class="form-group">
+        <label>Rating</label>
+        <select name="reviewRating">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+        <br>
+    </div>
+
+    <!-- Review Description -->
+    <div class="form-group">
+        <label>Review</label>
+        <input type="text" name="reviewDescription" placeholder="Write your review here.">
+        <br>
+    </div>
     
-    <!-- This loops through the products in the products table to display their names. -->
-    <label>Product</label>
-    <select name="productID">
-
-        <?php 
-            $result = $conn->query("SELECT productID FROM products") or die($conn->error());
-            while ($row = $result->fetch_assoc()):
-        ?>
-
-        <option><?php echo $row['productID']?></option>
-        
-        <?php endwhile; ?>
-
-    </select>
-
-    <label>Rating</label>
-    <select name="reviewRating">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-    </select>
-
-    <label>Review</label>
-    <input type="text" name="reviewDescription" placeholder="Write your review here.">
-
-    <button type="submit" name="save">Submit</button>
+    <!-- Submit Button -->
+    <div class="form-group">
+        <button type="submit" name="save" class="btn btn-primary">Submit</button>
+    </div>
 
 </form>
 
