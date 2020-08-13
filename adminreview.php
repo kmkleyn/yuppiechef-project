54e610d5c91fc0cb9c5532264ab31a1a8960a7a1
Kirstin Kleyn <57179572+kmkleyn@users.noreply.github.com>
2020-08-13 12:16:52 +0200

    Update adminreview.php
    Add Bootstrap styling and other minor edits.

--- adminreview.php
+++ adminreview.php
@@ -2,15 +2,18 @@
 	require_once 'includes/edit.inc.php';
     require_once 'includes/header.inc.php';
 ?>
+
+<h1>Edit Review</h1>
+
+<!-- This form allows the admin to edit the review description they clicked on in adminlisting.php. -->
 <form action="includes/editsave.inc.php" method="POST">
 
     <input type="hidden" name="reviewID" value="<?php echo $reviewID; ?>">
     <input type="text" name="reviewEdited" value="<?php echo $reviewDescription; ?>">
-    <button type="submit" name="save">Save</button>
+    <button type="submit" name="save" class="btn btn-primary">Save</button>
 
 </form>
 
 <?php
     require_once 'includes/footer.inc.php'
-
 ?>
